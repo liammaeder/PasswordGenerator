0ef15707eba8c464a2d5049c6835c57f57e6f88f
Monarkle <liam.maeder@gmail.com>
2022-02-15 11:24:45 +0200

    Commit angle bracket bug

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 var pCharset = "";
-var pswdString = "";
+var pswdOut = "";
 var cbSel = false;
 
 function generatePassword() {
@@ -9,14 +9,17 @@ function generatePassword() {
     const cUpper = document.getElementById("charUpper");
     const cLower = document.getElementById("charLower");
     const cSpecial = document.getElementById("charSpecial");
-    var specChar = "!@#$%^&*()_+{}|:\"<>?\[\]\;\',./";
+    var specChar = "!@#$%^&*()_+{}|:\"?\[\]\;\',./";
     var uppChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var lowChar = "abcdefghijklmnopqrstuvwxyz";
     var numChar = "0123456789";
+    var pswdString = "";
 
     if (pLength == 0) {
         alert("Please enter a password length");
         return;
+    } else if (pLength > 0) {
+        console.log("Password length: " + pLength);
     }
 
     if (cSpecial.checked) {
@@ -49,6 +52,13 @@ function generatePassword() {
         pswdString += pCharset.substring(rndIdx, rndIdx + 1);
     }
 
+    pswdOut = pswdString;
+    console.log("pswdOut length: " + pswdOut.length);
+    console.log("pswdOut: " + pswdOut);
+    console.log("pswdString length: " + pswdString.length);
+    console.log("pswdString: " + pswdString);
+    
+
     var resultBlk = document.getElementById("resultsPrint");
         resultBlk.innerHTML = "<p>Your password is:</p><br><p id=\"pswdString\" class=\"pswdString\">" + pswdString + "</p><br><button onclick=\"copyPswd()\">Copy to clipboard.</button>";
         resultBlk.hidden = false;
@@ -58,10 +68,12 @@ function generatePassword() {
 function clearForm() {
     document.getElementById("pswdForm").reset();
     document.getElementById("resultsPrint").hidden = true;
+    pswdOut = "";
+    console.clear();
 }
 
 function copyPswd() {
     navigator.clipboard.writeText(document.getElementById("pswdString").innerHTML);
     var resultBlk = document.getElementById("resultsPrint");
-    resultBlk.innerHTML ="<p>Your password is:</p><br><p id=\"pswdString\" class=\"pswdString\">" + pswdString + "</p><br><p>Copied!</p><br><button onclick=\"copyPswd()\">Copy to clipboard.</button>";
+    resultBlk.innerHTML ="<p>Your password is:</p><br><p id=\"pswdString\" class=\"pswdString\">" + pswdOut + "</p><br><p>Copied!</p><br><button onclick=\"copyPswd()\">Copy to clipboard.</button>";
 }
